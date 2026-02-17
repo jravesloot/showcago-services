be75a25be38b4d4ee53ae826639a57528a19feff
Jay Rave <industry.jhr@gmail.com>
2026-02-17 10:55:33 -0600

    Persist database to local filesystem

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,7 +10,7 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-alias ShowcagoServices.Events.Artist
+alias ShowcagoServices.Schema.Artist
 alias ShowcagoServices.Repo
 
 artists_csv_path = Application.app_dir(:showcago_services, "priv/repo/seeds/artists.csv")
