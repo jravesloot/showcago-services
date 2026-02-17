95f6c0734de391498f0daf25b3010740f5579004
Jay Rave <industry.jhr@gmail.com>
2026-02-17 11:14:49 -0600

    Add venues

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,6 +11,7 @@
 # and so on) as they will fail if something goes wrong.
 
 alias ShowcagoServices.Schema.Artist
+alias ShowcagoServices.Schema.Venue
 alias ShowcagoServices.Repo
 
 artists_csv_path = Application.app_dir(:showcago_services, "priv/repo/seeds/artists.csv")
@@ -51,4 +52,52 @@ else
     )
 
   IO.puts("Seeded #{inserted_count} artists from #{artists_csv_path}")
+end
+
+venues_csv_path = Application.app_dir(:showcago_services, "priv/repo/seeds/venues.csv")
+
+empty_to_nil = fn value ->
+  if value == "" do
+    nil
+  else
+    value
+  end
+end
+
+venues =
+  venues_csv_path
+  |> File.stream!([], :line)
+  |> Stream.map(&String.trim/1)
+  |> Stream.reject(&(&1 in ["", "Name,Address,City,State,ZipCode,Website"]))
+  |> Stream.map(fn line ->
+    [name, address, city, state, zip_code, website] = String.split(line, ",", parts: 6)
+
+    %{
+      name: name,
+      address: empty_to_nil.(address),
+      city: empty_to_nil.(city),
+      state: empty_to_nil.(state),
+      zip_code: empty_to_nil.(zip_code),
+      website: empty_to_nil.(website)
+    }
+  end)
+  |> Enum.uniq_by(& &1.name)
+
+if venues == [] do
+  IO.puts("No venues found in #{venues_csv_path}")
+else
+  now = DateTime.utc_now(:second)
+
+  rows =
+    Enum.map(venues, fn venue ->
+      Map.merge(venue, %{inserted_at: now, updated_at: now})
+    end)
+
+  {inserted_count, _} =
+    Repo.insert_all(Venue, rows,
+      on_conflict: :nothing,
+      conflict_target: [:name]
+    )
+
+  IO.puts("Seeded #{inserted_count} venues from #{venues_csv_path}")
 end
